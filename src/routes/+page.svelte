5ffb2e15c5f8453914ec08231ba39b6d1dd61530
Jeremia Kimelman <jbkimelman@gmail.com>
2023-08-29 17:24:59 -0700

    Use CSS grid to position the basic parts of the homagepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,9 +32,11 @@
   <div class="blocks-container">
     {#each blocks as block}
       <div class="block">
+        <div class="block-title">{block.label}</div>
         <div class="amount">{formatDollar(block.value)}</div>
-        <div class="label">
-          <a href={block.href}>{block.label}</a>
+        <div class="amount-label">Raised</div>
+        <div class="block-link">
+          <a href={block.href}>Learn more</a>
         </div>
       </div>
     {/each}
@@ -44,12 +46,70 @@
 
 <style lang="scss">
   h1 {
+    font-size: 36px;
     line-height: 1.1em;
     text-align: center;
   }
 
+  p {
+    margin: 0;
+  }
+
   .hero {
-    background-color: blue;
+    --column-gutter: 1rem;
+    display: grid;
+    grid-template-columns: var(--column-gutter) 2fr var(--column-gutter) 1fr var(--column-gutter) 1fr var(--column-gutter);
+    grid-template-rows: var(--column-gutter) 1fr var(--column-gutter) 1fr var(--column-gutter);
+    min-height: 50vh;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+
+  .tagline {
+    grid-column: 2/2;
+    grid-row: 2/2;
+  }
+
+  .total-raised-container {
+    border: 1px solid white;
+    grid-column: 2/2;
+    grid-row: 4/4;
+  }
+
+  .blocks-container {
+    display: grid;
+    grid-template-columns: 1fr var(--column-gutter) 1fr;
+    grid-template-rows: 1fr var(--column-gutter) 1fr;
+    grid-column: 4/7;
+    grid-row: 2/5;
+  }
+
+  .blocks-container p {
+    font-size: 14px;
+    grid-column: 1/4;
+    grid-row: 3/4;
+  }
+
+  .block {
+    align-items: center;
+    background-color: white;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .block:nth-child(2) {
+    grid-column: 3/4;
+  }
+
+  .block-title {
+    font-weight: 700;
+    text-align: center;
+  }
+
+  .block .amount {
+    color: cornflowerblue;
+    font-weight: 700;
   }
 
 </style>
