b4a86f920afde3b217e6080f99084ec086a1ee5e
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2023-09-01 23:45:25 -0700

    Show committee names instead of just IDs (#17)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  title: 'Sacramento campaign cash',
+  title: "Sacramento campaign cash",
   bodies: [
     {
       body: "sac-city",
@@ -7,72 +7,186 @@ const config = {
       legislators: [{
         name: "Darrell Steinberg",
         title: "Mayor",
-        committees: ["1396270", "1380886", "1388020", "1426136", "1237420"],
+        committees: [
+          {
+            id: "1380886",
+            name: "STEINBERG FOR SACRAMENTO MAYOR 2016",
+          },
+          {
+            id: "1388020",
+            name: "STEINBERG FOR SACRAMENTO MAYOR 2020",
+          },
+          {
+            id: "1426136",
+            name: "STEINBERG FOR SACRAMENTO MAYOR 2024",
+          }
+        ],
       }, {
         name: "Lisa Kaplin",
         title: "City Council, District 1",
-        committees: ["1442529"],
-      },
-      {
+        committees: [{
+          id: "1442529",
+          name: "LISA KAPLAN FOR CITY COUNCIL 2022",
+        }],
+      }, {
         name: "Sean Loloee",
         title: "City Council, District 2",
-        committees: ["1419806", "1441736"],
+        committees: [
+          {
+            id: "1419806",
+            name: "SEAN LOLOEE FOR CITY COUNCIL 2020",
+          },
+          {
+            id: "1441736",
+            name: "SEAN LOLOEE FOR CITY COUNCIL 2024",
+          },
+        ],
       }, {
         name: "Karina Talamantes",
         title: "City Council, District 3",
-        committees: ["1443537"],
-      },
-      {
+        committees: [
+          {
+            id: "1443537",
+            name: "KARINA TALAMANTES FOR CITY COUNCIL 2022",
+          },
+        ],
+      }, {
         name: "Katie Valenzuela",
         title: "City Council, District 4",
-        committees: ["1416339", "1426389"],
+        committees: [
+          {
+            id: "1416339",
+            name: "KATIE VALENZUELA FOR SACRAMENTO CITY COUNCIL 2020",
+          },
+          {
+            id: "1426389",
+            name: "KATIE VALENZUELA FOR SACRAMENTO CITY COUNCIL 2024",
+          },
+        ],
       }, {
         name: "Caity Maple",
         title: "City Council, District 5",
-        committees: ["1435034"],
-      },
-      {
+        committees: [
+          {
+            id: "1435034",
+            name: "CAITY MAPLE FOR CITY COUNCIL 2022",
+          },
+        ],
+      }, {
         name: "Eric Guerra",
         title: "City Council, District 6",
-        committees: ["1374232", "1368754", "1376768", "1396274", "1431012"],
+        committees: [
+          {
+            id: "1374232",
+            name: "ERIC GUERRA FOR CITY COUNCIL 2015",
+          },
+          {
+            id: "1368754",
+            name: "ERIC GUERRA FOR CITY COUNCIL 2016",
+          },
+          {
+            id: "1376768",
+            name: "ERIC GUERRA FOR CITY COUNCIL 2016",
+          },
+          {
+            id: "1396274",
+            name: "ERIC GUERRA FOR CITY COUNCIL 2020",
+          },
+          {
+            id: "1431012",
+            name: "ERIC GUERRA FOR CITY COUNCIL 2024",
+          },
+        ],
       }, {
         name: "Rick Jennings",
         title: "City Council, District 7",
-        committees: ["1359899", "1367862", "1443348"],
+        committees: [
+          {
+            id: "1359899",
+            name: "RICK JENNINGS FOR CITY COUNCIL 2014",
+          },
+          {
+            id: "1367862",
+            name: "RICK JENNINGS FOR CITY COUNCIL 2018",
+          },
+          {
+            id: "1443348",
+            name: "RICK JENNINGS FOR CITY COUNCIL 2022",
+          },
+        ],
       }, {
         name: "Mai Vang",
         title: "City Council, District 8",
-        committees: ["1381818", "1435334", "1420665"],
+        committees: [
+          {
+            id: "1381818",
+            name: "MAI VANG FOR SCHOOL BOARD 2016",
+          },
+          {
+            id: "1420665",
+            name: "MAI VANG FOR CITY COUNCIL 2020",
+          },
+          {
+            id: "1435334",
+            name: "MAI VANG FOR CITY COUNCIL 2024",
+          },
+        ],
       }],
       vendorId: "SAC",
     },
-    {    
+    {
       body: "sac-county",
       legislators: [{
         name: "Phil Sterna",
         title: "Supervisor, District 1",
-        committees: ["1334082", "1374881", "1422227"],
+        committees: [
+          {
+            id: "1334082",
+            name: "SERNA FOR SUPERVISOR 2014",
+          },
+          {
+            id: "1374881",
+            name: "SERNA FOR SUPERVISOR 2018",
+          },
+          {
+            id: "1422227",
+            name: "SERNA FOR SUPERVISOR 2022",
+          },
+        ],
       }, {
         name: "Patrick Kennedy",
         title: "Supervisor, District 2",
-        committees: ["1369509", "1409487", "1456505"],
+        committees: [
+          {
+            id: "1369509",
+            name: "PATRICK KENNEDY FOR SUPERVISOR 2018",
+          },
+          {
+            id: "1409487",
+            name: "PATRICK KENNEDY FOR SUPERVISOR 2022",
+          },
+          {
+            id: "1456505",
+            name: "PATRICK KENNEDY FOR SUPERVISOR 2026",
+          },
+        ],
       }, {
         name: "Rich Desmond",
         title: "Supervisor, District 3",
-        committees: ["1419486"],
+        committees: [{ id: "1419486", name: "RICH DESMOND FOR SUPERVISOR 2020, 2024" }],
       }, {
         name: "Sue Frost",
         title: "Supervisor, District 4",
-        committees: ["1380596"],
+        committees: [{ id: "1380596", name: "SUE FROST FOR SUPERVISOR 2016, 2020, 2024" }],
       }, {
         name: "Pat Hume",
         title: "Supervisor, District 5",
-        committees: ["1438939"],
+        committees: [{ id: "1438939", name: "HUME FOR SUPERVISOR 2022" }],
       }],
       name: "Board of Supervisors",
-      vendorId: "SCO", 
+      vendorId: "SCO",
     },
   ],
 };
 
-export default config
+export default config;
--- scripts/aggregate.js
+++ scripts/aggregate.js
@@ -1,74 +1,74 @@
-import _ from 'lodash'
-import { rollup, sum } from 'd3-array'
-import Queue from 'p-queue'
-import { PrismaClient } from '@prisma/client'
+import _ from "lodash";
+import { rollup, sum } from "d3-array";
+import Queue from "p-queue";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 function createContributorId(d) {
-    return [
-        d.contributorCommitteeId,
-        d.contributorLastName,
-        d.contributorFirstName,
-        d.contributorZip,
-    ].join('--')
+  return [
+    d.contributorCommitteeId,
+    d.contributorLastName,
+    d.contributorFirstName,
+    d.contributorZip,
+  ].join("--");
 }
 
 export default async function aggregate(legislators, body) {
-    const legislatorsWithContributors = []
-    const queue = new Queue({ concurrency: 2 })
+  const legislatorsWithContributors = [];
+  const queue = new Queue({ concurrency: 2 });
 
-    legislators.forEach(legislator => {
-        const { name, title, committees } = legislator
-        queue.add(async() => {
-            const scheduleA = await prisma.scheduleA.findMany({
-                where: {
-                    fppcId: { in: committees
-                    }
-                }
-            })
+  legislators.forEach((legislator) => {
+    const { name, title, committees } = legislator;
+    const committeeIds = committees.map((d) => d.id);
+    queue.add(async () => {
+      const scheduleA = await prisma.scheduleA.findMany({
+        where: {
+          fppcId: { in: committeeIds },
+        },
+      });
 
-            const rolled = rollup(scheduleA, contributions => {
-                const amount = sum(contributions, d => d.amount)
-                const {
-                    filerName,
-                    fppcId,
-                    contributorCommitteeId,
-                    contributorFirstName,
-                    contributorLastName,
-                    contributorCity,
-                    contributorState,
-                    contributorType,
-                    contributorZip,
-                } = contributions[0]
-                return {
-                    filerName,
-                    fppcId,
-                    contributorCommitteeId,
-                    contributorFirstName,
-                    contributorLastName,
-                    contributorCity,
-                    contributorState,
-                    contributorType,
-                    contributorZip,
-                    amount
-                }
-            }, createContributorId)
+      const rolled = rollup(scheduleA, (contributions) => {
+        const amount = sum(contributions, (d) => d.amount);
+        const {
+          filerName,
+          fppcId,
+          contributorCommitteeId,
+          contributorFirstName,
+          contributorLastName,
+          contributorCity,
+          contributorState,
+          contributorType,
+          contributorZip,
+        } = contributions[0];
+        return {
+          filerName,
+          fppcId,
+          contributorCommitteeId,
+          contributorFirstName,
+          contributorLastName,
+          contributorCity,
+          contributorState,
+          contributorType,
+          contributorZip,
+          amount,
+        };
+      }, createContributorId);
 
-            const contributors = Array.from(rolled).map(d => d[1])
-            const sorted = _.orderBy(contributors, ['amount'], ['desc'])
+      const contributors = Array.from(rolled).map((d) => d[1]);
+      const sorted = _.orderBy(contributors, ["amount"], ["desc"]);
 
-            legislatorsWithContributors.push({
-                name,
-                title,
-                body,
-                committees,
-                contributors: sorted
-            })
-        })
-    })
+      legislatorsWithContributors.push({
+        name,
+        title,
+        body,
+        committees,
+        contributors: sorted,
+      });
+    });
+  });
 
-    await queue.onIdle()
+  await queue.onIdle();
 
-    return legislatorsWithContributors
+  return legislatorsWithContributors;
 }
