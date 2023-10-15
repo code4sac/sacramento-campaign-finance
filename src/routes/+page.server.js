aa81fca9408ccbb32c4c6519ecb119372e645337
Jeremia Kimelman <jeremia@calmatters.org>
2023-10-15 13:06:17 -0700

    Use bootstrap classes for block totals of upcoming election, city, and county
    Trying to use Bootstrap when possible since it's more documented than our burgeoning style scheme and simplified the layout with three blocks and no more total (since it doesn't really matter just how much is in the system when we are doing elections that change over time)

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -11,16 +11,41 @@ export function load() {
         }).flat()
         const matching = data.filter(d => legislatorsCommitteeIds.includes(d.fppcId))
         const total = sum(matching, d => d.amount)
+        let offices = ['Mayor', 'City councilmembers']
+
+        if (body === 'sac-county') {
+            offices = ['Supervisors']
+        }
         return {
             body,
+            href: `/body/${body}`,
             name,
             total,
-            officials: []
+            offices,
         }
     })
 
+    const upcomingElection = config.elections.find(d => d.year === 2024)
+    const allElectionCommitteeIds = upcomingElection.contests.map(contest => {
+        const candidateIds = contest.candidates.map(d => d.committee.id)
+        return candidateIds
+    }).flat()
+
+    const electionData = data.filter(d => {
+        return allElectionCommitteeIds.includes(d.fppcId)
+    })
+    const electionTotal = sum(electionData, d => d.amount)
+
+    const election = {
+        body: '',
+        href: `/election/2024`,
+        name: 'Upcoming election',
+        total: electionTotal,
+        offices: ['Mayor', 'City councilmembers (D2, D4, D6, D8)']
+    }
+
     return {
         generated,
-        totals
+        totals: [election, ...totals]
     }
 }
