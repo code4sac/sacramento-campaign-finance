edf1492e478af2902e79c7b9ac66fa9c4d1d2bc1
Jeremia Kimelman <jbkimelman@gmail.com>
2024-11-03 11:06:20 -0800

    Handle case where we were double counting some contributions in the upcoming election

--- scripts/load.js
+++ scripts/load.js
@@ -1,10 +1,22 @@
-import fs from 'fs/promises';
-import Queue from 'p-queue';
-import { rollup } from 'd3-array'
-import _ from 'lodash';
+import fs from "fs/promises";
+import Queue from "p-queue";
+import { rollup } from "d3-array";
+import _ from "lodash";
 
-const years = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
-const schedules = ['schedule-a', 'schedule-c', '497'];
+const years = [
+    2014,
+    2015,
+    2016,
+    2017,
+    2018,
+    2019,
+    2020,
+    2021,
+    2022,
+    2023,
+    2024,
+];
+const schedules = ["schedule-a", "schedule-c", '497'];
 
 export default async function load() {
     const data = [];
@@ -14,15 +26,15 @@ export default async function load() {
         schedules.forEach((schedule) => {
             let type = null;
 
-            if (schedule === 'schedule-a') {
-                type = 'monetary';
-            } else if (schedule === 'schedule-c') {
-                type = 'non-monetary';
-            } else if (schedule === '497') {
-                type = 'late-contribution'
+            if (schedule === "schedule-a") {
+                type = "monetary";
+            } else if (schedule === "schedule-c") {
+                type = "non-monetary";
+            } else if (schedule === "497") {
+                type = "late-contribution";
             }
 
-            const fileName = `data/${schedule}-${year}.json`
+            const fileName = `data/${schedule}-${year}.json`;
             queue.add(async () => {
                 try {
                     const d = await fs.readFile(fileName);
@@ -30,12 +42,12 @@ export default async function load() {
                     const withSchedule = j.map((d) => {
                         return {
                             ...d,
-                            type
+                            type,
                         };
                     });
                     data.push(...withSchedule);
                 } catch (e) {
-                    console.error(`Problem with ${fileName}`)
+                    console.error(`Problem with ${fileName}`);
                 }
             });
         });
@@ -43,14 +55,28 @@ export default async function load() {
 
     await queue.onIdle();
 
+    const rolled = rollup(
+        data,
+        (values) => {
+            const sorted = _.orderBy(values, ["reportDate"], ["desc"]);
+            return sorted[0];
+        },
+        (d) =>
+            `${d.fppcId} ${d.transactionId} ${d.date} ${d.amount} ${d.contributorLastName}`,
+    );
+    const arr = []
+    rolled.forEach((d, key) => {
+        arr.push(d)
+    })
+
     // tested out this apporach in this
     // notebook: https://observablehq.com/d/4d3ee045f9885b59
-    const withThroughDate = data.filter(d => d.reportThruDate)
+    const withThroughDate = arr.filter(d => d.reportThruDate)
     const latestThroughDateByFiler = rollup(withThroughDate, values => {
         const throughDates = _.uniq(values.map((d) => d.reportThruDate)).sort();
         return throughDates.pop();
     }, d => d.fppcId)
-    const onlyInclude497AfterLatestThroughDate = data.filter(d => {
+    const onlyInclude497AfterLatestThroughDate = arr.filter(d => {
         if (d.type !== 'late-contribution') return true
         const latestThroughDate = latestThroughDateByFiler.get(d.fppcId);
         return d.date > latestThroughDate;
